0519f0ec95379077bb93f4d502e2ac90ded945e9
Stick-z <sticknameful@gmail.com>
2020-04-19 19:53:45 -0700

    Removed .now folder

--- server.js
+++ server.js
@@ -16,15 +16,6 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use("/api/items", itemRouter);
 
-//serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 //connect to mongo
 mongoose
   .connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
